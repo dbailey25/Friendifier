d7bc64ec4d2120dd3695fb17c79b7e43989ef14f
dbailey25 <dtbailey25@gmail.com>
2018-03-01 21:34:11 -0500

    capture user input

---
+++ app/data/friends.js
@@ -0,0 +1,26 @@
+var userArray =
+[{
+  "name":"Kari",
+  "photo":"https://photos.google.com/album/AF1QipNoIWz86Dyv8pP9_LY41nXQXzUiv8yQjHPFFGzf/photo/AF1QipNl9G_gH1yfq-iDaPhcWil9Rk-2O10JQyMVukwm",
+  "scores":[5,1,4,4,5,1,2,5,4,1]
+},{
+  "name":"Demian",
+  "photo":"https://photos.google.com/album/AF1QipNoIWz86Dyv8pP9_LY41nXQXzUiv8yQjHPFFGzf/photo/AF1QipNXR-xLqDFb9eq6V_JH9T2FkXbWthJKqvUkNZZ9",
+  "scores":[3,2,2,5,5,1,1,5,4,2]
+},{
+  "name":"Alex",
+  "photo":"https://photos.google.com/album/AF1QipNoIWz86Dyv8pP9_LY41nXQXzUiv8yQjHPFFGzf/photo/AF1QipMDqkOj0OjgWQDsDQVJvhEtuIghI3lwDUu2MvF7",
+  "scores":[5,4,5,3,4,2,1,4,1,4]
+},{
+  "name":"Tamara",
+  "photo":"https://photos.google.com/album/AF1QipNoIWz86Dyv8pP9_LY41nXQXzUiv8yQjHPFFGzf/photo/AF1QipMIBwpf2uDoHLBoVnjh4GNm3iDe8Zda4yhyX4gk",
+  "scores":[1,4,3,3,2,5,5,4,1,3]
+},{
+  "name":"Heather",
+  "photo":"https://photos.google.com/album/AF1QipNoIWz86Dyv8pP9_LY41nXQXzUiv8yQjHPFFGzf/photo/AF1QipOTvQ6z7I6jIUpQgw1dbO8oSKBIlv0BhEyODqM7",
+  "scores":[2,2,5,5,4,3,5,5,3,2]
+},{
+  "name":"Van",
+  "photo":"https://photos.google.com/album/AF1QipNoIWz86Dyv8pP9_LY41nXQXzUiv8yQjHPFFGzf/photo/AF1QipOlB19cKx40loGkIxFhfEUIneQcnc-reaEPko7_",
+  "scores":[4,1,1,3,2,5,2,1,3,4]
+}]
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,20 @@
+module.exports = function(app) {
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friendData);
+  });
+
+  app.post("/api/friends", function(req, res) {
+
+    
+
+    // if (tableData.length < 5) {
+    //   tableData.push(req.body);
+    //   res.json(true);
+    // }
+    // else {
+    //   waitListData.push(req.body);
+    //   res.json(false);
+    // }
+  });
+};
