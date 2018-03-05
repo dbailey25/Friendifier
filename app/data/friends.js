d0b769f84f7f7a6f4843f6d3b5357159eb5bdfe1
dbailey25 <dtbailey25@gmail.com>
2018-03-04 20:50:09 -0500

    bug fix: display match photo

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,27 @@
 var friendArray =
 [{
   "name":"Kari",
-  "photo":"../data/images/Kari.jpg",
+  "photo":"https://i.imgur.com/E1rqkGI.jpg",
   "scores":[5,5,5,5,5,5,5,5,5,5]
 },{
   "name":"Demian",
-  "photo":"../data/images/Demian.jpg",
+  "photo":"https://i.imgur.com/ZowlwLV.jpg",
   "scores":[2,2,2,2,2,2,2,2,2,2]
 },{
   "name":"Alex",
-  "photo":"../data/images/Alex.jpg",
+  "photo":"https://i.imgur.com/fZCumg4.jpg",
   "scores":[4,4,4,4,4,4,4,4,4,4]
 },{
   "name":"Tamara",
-  "photo":"../data/images/Tamara.jpg",
+  "photo":"https://i.imgur.com/CzcKYJr.jpg",
   "scores":[1,4,2,3,2,5,5,4,1,3]
 },{
   "name":"Heather",
-  "photo":"../data/images/Heather.jpg",
+  "photo":"https://i.imgur.com/oYIuGCL.jpg",
   "scores":[1,1,1,1,1,1,1,1,1,1]
 },{
   "name":"Van",
-  "photo":"../data/images/Van.jpg",
+  "photo":"https://i.imgur.com/wldahne.jpg",
   "scores":[3,3,3,3,3,3,3,3,3,3]
 }];
 
